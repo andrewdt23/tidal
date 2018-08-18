32b69f4f99d67ee70889d76c25b3f7d661a16c5d
Andrew Thompson <Andrew@Andrews-MacBook-Pro-3.local>
2018-08-17 21:56:27 -0700

    Add background image and site title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
-import Header from './components/headerComponent/header';
-import Footer from './components/footerComponent/footer';
-import Homepage from './components/pages/homePage';
-import Photography from './components/pages/photography';
-import Contact from './components/pages/contact';
+import Homepage from './components/homePage.js';
 import './Assets/css/default.min.css';
 import {
     BrowserRouter as Router, Route,
@@ -13,11 +9,7 @@ class App extends Component {
     render() {
         return (<Router>
             <div className="App">
-                <Header/>
-                <Route exact="exact" path='/' component={Homepage}/>
-                <Route exact="exact" path='/Photography' component={Photography}/>
-                <Route exact="exact" path='/Contact' component={Contact}/>
-                <Footer/>
+                <Homepage />
             </div>
         </Router>);
     }
