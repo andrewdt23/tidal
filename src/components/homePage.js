206f85992f252dd0006dea9e748cfd172cfd4f3e
Andrew Thompson <andrewdt23@Andrews-MacBook-Pro.local>
2019-03-14 18:42:03 -0700

    Got "enter location" working and styled

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,104 +1,189 @@
 import React, {Component} from 'react';
-import { Modal, Button} from 'react-bootstrap'
+import {Modal, Button} from 'react-bootstrap'
 
 class Homepage extends Component {
 
     constructor() {
         super();
-        this.handleClose = this.handleClose.bind(this);
-        this.handleOpen = this.handleOpen.bind(this);
-        this.componentDidMount = this.componentDidMount.bind(this);
+        this.handleTideClose = this.handleTideClose.bind(this);
+        this.handleTideOpen = this.handleTideOpen.bind(this);
+        this.handleInputClose = this.handleInputClose.bind(this);
+        this.handleInputOpen = this.handleInputOpen.bind(this);
+        this.computeTides = this.computeTides.bind(this);
+        this.formatTides = this.formatTides.bind(this);
+        this.updateLocationInput = this.updateLocationInput.bind(this);
+        this.handleInputSubmit = this.handleInputSubmit.bind(this);
+        this.getTides = this.getTides.bind(this);
         this.state = {
-          showModal: false,
+          showTideModal: false,
+          showInputModal: false,
           lowTide: null,
-          highTide: null
+          highTide: null,
+          location: null,
         };
     }
 
-    componentDidMount() {
+    computeTides(tideList) {
+        let highTide = [];
+        let lowTide = [];
+        tideList.forEach((entry, i) => {
+            if(i === 23 || i === 0) {
+                return;
+            }
+            if(tideList[i+1].tide < entry.tide && tideList[i-1].tide < entry.tide) {
+                highTide.push(entry);
+            }
+            if(tideList[i-1].tide > entry.tide && tideList[i+1].tide > entry.tide) {
+                lowTide.push(entry);
+            }
+        });
+
+        if(highTide.length < 2) {
+            highTide.push({hour: '12AM', tide: 0});
+        }
+        if(lowTide.length < 2) {
+            lowTide.push({hour: '12AM', tide: 0});
+        }
+
+        return({highTide: highTide, lowTide: lowTide});
+    }
+
+    formatTides(highTide, lowTide) {
+        let lowTideTimes = ''
+        lowTide.forEach((entry, i) => {
+            if(i === 0) {
+                lowTideTimes = lowTideTimes + entry.hour + ' and ';
+            } else {
+                lowTideTimes = lowTideTimes + entry.hour;
+            }
+        });
+        let highTideTimes = ''
+        highTide.forEach((entry, i) => {
+            if(i === 0) {
+                highTideTimes = highTideTimes + entry.hour + ' and ';
+            } else {
+                highTideTimes = highTideTimes + entry.hour;
+            }
+        });
+
+        return({highTideTimes: highTideTimes, lowTideTimes: lowTideTimes});
+    }
+
+    getTides() {
         const request = new XMLHttpRequest();
 
-        request.open('GET', 'https://cors-anywhere.herokuapp.com/http://api.spitcast.com/api/county/tide/orange-county/', true);
-        request.onload = () => {
-            // Begin accessing JSON data here
-            const data = JSON.parse(request.response);
+        if(this.state.location) {
+            let county = this.state.location.replace(/\s+/g, '-').toLowerCase();
+            let url = 'https://cors-anywhere.herokuapp.com/http://api.spitcast.com/api/county/tide/' + county + '/'
 
-            if (request.status >= 200 && request.status < 400) {
-                let tideList = [];
-                // console.log(data);
-                data.forEach((entry, i) => {
-                    if (entry.hour && entry.tide) {
-                        // console.log(hour.tide);
-                        if (tideList && i !== 24) {
-                            tideList.push({hour: entry.hour, tide: entry.tide});
+            request.open('GET', url, true);
+            request.onload = () => {
+                // Begin accessing JSON data here
+                const data = JSON.parse(request.response);
+
+                if (request.status >= 200 && request.status < 400) {
+                    let tideList = [];
+                    // console.log(data);
+                    data.forEach((entry, i) => {
+                        if (entry.hour && entry.tide) {
+                            // console.log(hour.tide);
+                            if (tideList && i !== 24) {
+                                tideList.push({hour: entry.hour, tide: entry.tide});
+                            }
                         }
-                    }
-                });
-                // console.log(tideList);
-                let highTide = [];
-                let lowTide = [];
-                tideList.forEach((entry, i) => {
-                    if(i === 23 || i === 0) {
-                        return;
-                    }
-                    if(tideList[i+1].tide < entry.tide && tideList[i-1].tide < entry.tide) {
-                        highTide.push(entry)
-                    }
-                    if(tideList[i-1].tide > entry.tide && tideList[i+1].tide > entry.tide) {
-                        lowTide.push(entry)
-                    }
-                });
-                // console.log(highTide);
-                // console.log(lowTide);
-                let lowTideTimes = ''
-                lowTide.forEach((entry, i) => {
-                    if(i === 0) {
-                        lowTideTimes = lowTideTimes + entry.hour + ' and ';
-                    } else {
-                        lowTideTimes = lowTideTimes + entry.hour;
-                    }
-                });
-                let highTideTimes = ''
-                highTide.forEach((entry, i) => {
-                    if(i === 0) {
-                        highTideTimes = highTideTimes + entry.hour + ' and ';
-                    } else {
-                        highTideTimes = highTideTimes + entry.hour;
-                    }
-                });
-                this.setState({
-                    lowTideTimes: lowTideTimes,
-                    highTideTimes: highTideTimes
-                });
-            } else {
-                console.log('error in fetching tides');
-            }
-        };
-        request.send();
+                    });
+
+                    let tides = this.computeTides(tideList);
+                    let highTide = tides.highTide;
+                    let lowTide = tides.lowTide;
+
+                    let tideTimes = this.formatTides(highTide, lowTide);
+                    let highTideTimes = tideTimes.highTideTimes;
+                    let lowTideTimes = tideTimes.lowTideTimes;
+
+                    this.setState({
+                        lowTideTimes: lowTideTimes,
+                        highTideTimes: highTideTimes,
+                        showTideModal: true
+                    });
+
+                } else {
+                    console.log('error in fetching tides');
+                }
+            };
+            request.send();
+        }
     }
 
-    handleClose() {
-        this.setState({showModal: false});
+    handleInputSubmit() {
+        this.getTides();
+        this.setState({showInputModal: false});
     }
 
-    handleOpen() {
-        this.setState({showModal: true});
+    updateLocationInput(event) {
+        event.preventDefault();
+        this.setState({location: event.target.value});
     }
 
-    renderModal() {
+    handleTideClose() {
+        this.setState({showTideModal: false});
+    }
+
+    handleTideOpen() {
+        this.setState({showTideModal: true});
+    }
+
+    handleInputClose() {
+        this.setState({showInputModal: false});
+    }
+
+    handleInputOpen() {
+        this.setState({showInputModal: true});
+    }
+
+    renderTideModal() {
         return (
             <div>
-                <Modal className="info-modal" show={this.state.showModal} onHide={this.handleClose}>
+                <Modal className="tide-modal" show={this.state.showTideModal} onHide={this.handleTideClose}>
                     <Modal.Header closeButton={true}>
                         <Modal.Title>Tide Information</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <p>
-                            Low Tide: {this.state.lowTideTimes}, High Tide: {this.state.highTideTimes}
+                            Low Tide: {this.state.lowTideTimes}
+                        </p>
+                        <p>
+                            High Tide: {this.state.highTideTimes}
                         </p>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button onClick={this.handleClose}>Close</Button>
+                        <Button onClick={this.handleTideClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
+
+    renderInputModal() {
+        return (
+            <div>
+                <Modal className="input-modal" show={this.state.showInputModal} onHide={this.handleInputClose}>
+                    <Modal.Header closeButton={true}>
+                        <Modal.Title>County Tide Information</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <form>
+                        <div className="enter-county">Enter county:</div>
+                        <div className="county-input-wrapper">
+                            <input className="county-input" type="text" onChange={this.updateLocationInput}/>
+                        </div>
+                        <div className="county-submit-wrapper">
+                            <Button className="county-submit" onClick={this.handleInputSubmit}>Submit</Button>
+                        </div>
+                    </form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.handleInputClose}>Close</Button>
                     </Modal.Footer>
                 </Modal>
             </div>
@@ -112,10 +197,10 @@ class Homepage extends Component {
                     TIDAL
                 </div>
                 <div className='buttons'>
-                    <Button className='county' onClick={this.handleOpen}>
+                    <Button className='county' onClick={this.handleInputOpen}>
                         ENTER LOCATION
                     </Button>
-                    <Button className='location' onClick={this.handleOpen}>
+                    <Button className='location' onClick={this.handleTideOpen}>
                         USE MY LOCATION
                     </Button>
                 </div>
@@ -124,7 +209,8 @@ class Homepage extends Component {
 
     render() {
         return (<div>
-            {this.renderModal()}
+            {this.renderTideModal()}
+            {this.renderInputModal()}
             {this.renderContent()}
         </div>);
     }
